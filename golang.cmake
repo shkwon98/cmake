0ccc848f2b1938b9213ef60aae97b177338ea116
shkwon98 <seonghyeon.kwon@avikus.ai>
2024-07-10 18:02:34 +0900

    Refactor golang integration

--- golang.cmake
+++ golang.cmake
@@ -1,231 +1,146 @@
 # Usage: add_go_executable(target)
 function(add_go_executable NAME)
-  go_build_envs()
+    go_build_envs()
 
-  file(
-    GLOB GO_SOURCE
-    RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}"
-    "*.go")
-  set(GO_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${NAME})
-  add_custom_command(
-    OUTPUT ${GO_OUTPUT}
-    COMMAND env ${GO_ENVS} ${CMAKE_Go_COMPILER} build -o "${CMAKE_CURRENT_BINARY_DIR}/${NAME}"
-            ${CMAKE_GO_FLAGS} ${GO_SOURCE}
-    COMMENT "go build ${NAME}"
-    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
+    file(GLOB GO_SOURCE RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "*.go")
+    set(GO_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${NAME})
 
-  add_custom_target(${NAME} ALL DEPENDS ${GO_OUTPUT})
+    add_custom_command(
+        OUTPUT ${GO_OUTPUT}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+        COMMAND GO_ENVS=${GO_ENVS}
+                ${CMAKE_Go_COMPILER} build -o "${CMAKE_CURRENT_BINARY_DIR}/${NAME}" ${GO_SOURCE})
+
+    add_custom_target(${NAME} ALL DEPENDS ${GO_OUTPUT})
 endfunction(add_go_executable)
 
 # Usage: add_cgo_executable(target)
 function(add_cgo_executable NAME)
-  set(multiValueArgs IMPORT)
-  cmake_parse_arguments(add_cgo_executable "" "" "${multiValueArgs}" ${ARGN})
+    go_build_envs()
 
-  cgo_fetch_cflags_and_ldflags(add_cgo_executable_IMPORT)
-  cgo_build_envs()
+    file(GLOB GO_SOURCE RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "*.go")
+    set(CGO_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${NAME})
 
-  file(
-    GLOB GO_SOURCE
-    RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}"
-    "*.go")
-  set(CGO_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${NAME})
-  add_custom_command(
-    OUTPUT ${CGO_OUTPUT}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    COMMENT "Building CGO modules for ${NAME}"
-    COMMAND ${CMAKE_COMMAND} -E remove ${CGO_OUTPUT}
-    COMMAND env ${CGO_ENVS} ${CMAKE_Go_COMPILER} build -o ${CMAKE_CURRENT_BINARY_DIR}/${NAME}
-            ${CMAKE_GO_FLAGS} ${GO_SOURCE}
-    DEPENDS ${CGO_DEPS_HANDLED})
+    add_custom_command(
+        OUTPUT ${CGO_OUTPUT}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+        # COMMENT "CGO_CFLAGS: $<TARGET_PROPERTY:${NAME},CGO_CFLAGS>"
+        COMMENT "CGO_LDFLAGS: $<TARGET_PROPERTY:${NAME},CGO_LDFLAGS>"
+        COMMAND ${CMAKE_COMMAND} -E remove ${CGO_OUTPUT}
+        COMMAND GO_ENVS=${GO_ENVS}
+                CGO_ENABLED=1
+                CC=${CMAKE_C_COMPILER}
+                CGO_CFLAGS=$<TARGET_PROPERTY:${NAME},CGO_CFLAGS>
+                CGO_LDFLAGS=$<TARGET_PROPERTY:${NAME},CGO_LDFLAGS>
+                ${CMAKE_Go_COMPILER} build -o ${CGO_OUTPUT} ${GO_SOURCE})
 
-  add_custom_target(${NAME} ALL DEPENDS ${CGO_OUTPUT})
+    add_custom_target(${NAME} ALL DEPENDS ${CGO_OUTPUT})
 endfunction()
 
-# Usage: target_go_get(target PACKAGE import1 import2 ...)
+# Usage: target_go_get(target PACKAGE package1 package2 ...)
 function(target_go_get NAME)
-  set(multiValueArgs PACKAGE)
-  cmake_parse_arguments(target_go_get "" "" "${multiValueArgs}" ${ARGN})
+    set(multiValueArgs PACKAGE)
+    cmake_parse_arguments(target_go_get "" "" "${multiValueArgs}" ${ARGN})
 
-  add_custom_target(
-    ${NAME}_GO_GET
-    COMMAND ${CMAKE_Go_COMPILER} get ${target_go_get_PACKAGE}
-    COMMENT "go get ${target_go_get_PACKAGE}"
-    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-    VERBATIM)
-  add_dependencies(${NAME} ${NAME}_GO_GET)
+    add_custom_target(${NAME}_GO_GET
+        COMMENT "go get ${target_go_get_PACKAGE}"
+        COMMAND ${CMAKE_Go_COMPILER} get ${target_go_get_PACKAGE}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+        VERBATIM)
+    add_dependencies(${NAME} ${NAME}_GO_GET)
 endfunction(target_go_get)
 
-set(CGO_RESOLVE_BLACKLIST pthread rt gcov systemd)
+# Usage: target_cgo_link_libraries(target IMPORT lib1 lib2 ...)
+function(target_cgo_link_libraries NAME)
+    set(multiValueArgs IMPORT)
+    cmake_parse_arguments(target_cgo_link_libraries "" "" "${multiValueArgs}" ${ARGN})
 
-set(CGO_CFLAGS_BLACKLIST
-    "-Werror"
-    "-Wall"
-    "-Wextra"
-    "-Wold-style-definition"
-    "-fdiagnostics-color=always"
-    "-Wformat-nonliteral"
-    "-Wformat=2")
+    cgo_fetch_cflags_and_ldflags(${target_cgo_link_libraries_IMPORT} ${NAME}_CFLAG_LIST ${NAME}_LDFLAG_LIST)
+    if(CMAKE_INSTALL_RPATH)
+        list(APPEND ${NAME}_LDFLAG_LIST -Wl,-rpath,${CMAKE_INSTALL_RPATH})
+    endif()
+
+    message(STATUS "CFLAGS: ${${NAME}_CFLAG_LIST}")
+    message(STATUS "LDFLAGS: ${${NAME}_LDFLAG_LIST}")
+
+    string(REPLACE ";" " " CGO_CFLAGS "${${NAME}_CFLAG_LIST}")
+    string(REPLACE ";" " " CGO_LDFLAGS "${${NAME}_LDFLAG_LIST}")
+
+    # Set the properties on the target to be used by the custom command
+    set_target_properties(${NAME} PROPERTIES
+        CGO_CFLAGS "${CGO_CFLAGS}"
+        CGO_LDFLAGS "${CGO_LDFLAGS}")
+    
+    # Create a custom target for dependencies
+    add_custom_target(${NAME}_CGO_LIBRARIES)
+    add_dependencies(${NAME} ${NAME}_CGO_LIBRARIES)
+endfunction()
+
 
 macro(go_build_envs)
-  # Assume ARM7 if on ARM
-  if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "arm")
-    list(APPEND GO_ENVS GOARCH=arm GOARM=7)
-  endif()
+    # Assume ARM7 if on ARM
+    if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "arm")
+        list(APPEND GO_ENVS GOARCH=arm GOARM=7)
+    endif()
 
-  # cross compile
-  if(CMAKE_CROSSCOMPILING)
-    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-      list(APPEND GO_ENVS GOOS=linux)
+    # cross compile
+    if(CMAKE_CROSSCOMPILING)
+        if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+            list(APPEND GO_ENVS GOOS=linux)
+        endif()
+        if(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+            list(APPEND GO_ENVS GOARCH=arm64)
+        endif()
     endif()
-    if(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
-      list(APPEND GO_ENVS GOARCH=arm64)
-    endif()
-  endif()
 endmacro()
 
-macro(cgo_build_envs)
-  set(CGO_ENVS CGO_ENABLED=1 CC=${CMAKE_C_COMPILER} CGO_CFLAGS="${CGO_CFLAGS}"
-               CGO_LDFLAGS="${CGO_LDFLAGS}")
 
-  # Assume ARM7 if on ARM
-  if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "arm")
-    list(APPEND CGO_ENVS GOARCH=arm GOARM=7)
-  endif()
+macro(cgo_fetch_cflags_and_ldflags TARGET CFLAG_LIST LDFLAG_LIST)
+    message(STATUS "Fetching CFLAGS and LDFLAGS for ${TARGET}")
 
-  # cross compile
-  if(CMAKE_CROSSCOMPILING)
-    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-      list(APPEND CGO_ENVS GOOS=linux)
-    endif()
-    if(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
-      list(APPEND CGO_ENVS GOARCH=arm64)
-    endif()
-  endif()
-endmacro()
-
-macro(cgo_fetch_cflags_and_ldflags CGO_DEPS)
-  set(CGO_LDFLAGS "")
-  set(CGO_CFLAGS "")
-
-  set(CGO_DEPS_STACK ${${CGO_DEPS}})
-  set(CGO_DEPS_HANDLED "")
-  set(CGO_RPATHS "")
-
-    foreach(L ${CGO_DEPS_STACK})
-      # Skip if already handled once
-      if("${L}" IN_LIST CGO_DEPS_HANDLED)
-        continue()
-      endif()
-
-      # Don't resolve system libs
-      if("${L}" IN_LIST CGO_RESOLVE_BLACKLIST)
-        continue()
-      endif()
-
-      # Resolve PkgConfig libs
-      if("${L}" MATCHES "PkgConfig")
-        get_target_property(L_INTERFACE_LIB ${L} INTERFACE_LINK_LIBRARIES)
-
-        # Mark handled
-        list(APPEND CGO_DEPS_HANDLED ${L})
-
-        # Finally override L
-        set(L "${L_INTERFACE_LIB}")
-
-        # L might be a list so iterate over it when adding rpaths
-        foreach(L_ITEM ${L})
-          # Fetch directory and add it to rpath-link if not already added
-          get_filename_component(R "${L_ITEM}" DIRECTORY)
-
-          if(NOT "${R}" IN_LIST CGO_RPATHS)
-            # Linker may need to find private libraries in the same directory
-            list(APPEND CGO_RPATHS "${R}")
-          endif()
-        endforeach()
-
-        # Add libraries to linker flags
-        list(APPEND CGO_LDFLAGS ${L})
-      else()
-        # Try resolve alias
-        get_target_property(L_ALIASED ${L} ALIASED_TARGET)
-        if(NOT "${L_ALIASED}" MATCHES "NOTFOUND")
-          set(L "${L_ALIASED}")
+    # In case the given library is not a cmake target. e.g. pthread, rt, etc.
+    if(NOT TARGET ${TARGET})
+        if(NOT -l${TARGET} IN_LIST ${LDFLAG_LIST})
+            message(STATUS "Adding -l${TARGET} to LDFLAGS")
+            list(APPEND ${LDFLAG_LIST} -l${TARGET})
         endif()
 
-        # Mark handled
-        list(APPEND CGO_DEPS_HANDLED ${L})
+    # In case the given library is a cmake target
+    else()
+        get_target_property(LIBS ${TARGET} LINK_LIBRARIES)
+        # Recursively fetch the flags from the dependencies
+        if(LIBS)
+            cgo_fetch_cflags_and_ldflags(${LIBS} ${CFLAG_LIST} ${LDFLAG_LIST})
+        endif()
 
-        get_target_property(L_INCLUDES ${L} INCLUDE_DIRECTORIES)
-        get_target_property(L_BUILD_DIR ${L} BINARY_DIR)
+        get_target_property(TYPE ${TARGET} TYPE)
+        # In case the given library is an INTERFACE_LIBRARY. e.g. Threads::Threads, etc.
+        if(TYPE STREQUAL "INTERFACE_LIBRARY")
+            get_target_property(LIB ${TARGET} INTERFACE_LINK_LIBRARIES)
+            if(NOT ${LIB} IN_LIST ${LDFLAG_LIST})
+                list(APPEND ${LDFLAG_LIST} ${LIB})
+            endif()
+        
+        # In case the given library is a SHARED or STATIC library.
+        else()
+            # "-I" flags for CGO
+            get_target_property(INCLUDE_DIR ${TARGET} INTERFACE_INCLUDE_DIRECTORIES)
+            foreach(I ${INCLUDE_DIR})
+                if(NOT -I${I} IN_LIST ${CFLAG_LIST})
+                    list(APPEND ${CFLAG_LIST} -I${I})
+                endif()
+            endforeach()
 
-        list(APPEND CGO_LDFLAGS -L${L_BUILD_DIR})
-        list(APPEND CGO_LDFLAGS -l${L})
+            # "-L" flags for CGO
+            get_target_property(LIB_DIR ${TARGET} BINARY_DIR)
+            if(NOT -L${LIB_DIR} IN_LIST ${LDFLAG_LIST})
+                list(APPEND ${LDFLAG_LIST} -L${LIB_DIR})
+            endif()
 
-        foreach(I ${L_INCLUDES})
-          list(APPEND CGO_CFLAGS -I${I})
-        endforeach()
-
-        list(REMOVE_ITEM CGO_DEPS_STACK "${L}")
-
-        get_target_property(DEPS ${L} LINK_LIBRARIES)
-        foreach(D ${DEPS})
-          list(APPEND CGO_DEPS_STACK "${D}")
-        endforeach()
-
-      endif()
-
-    endforeach()
-
-  # Adding cflags and ldflags ##### Must split sentences into CMake List before
-  # adding cflags and ldflags
-  if(CMAKE_C_FLAGS)
-    string(REPLACE " " ";" CMAKE_C_FLAGS_LIST ${CMAKE_C_FLAGS})
-    list(APPEND CGO_CFLAGS ${CMAKE_C_FLAGS_LIST})
-  else()
-    list(APPEND CGO_CFLAGS "")
-  endif()
-
-  if(CMAKE_EXE_LINKER_FLAGS)
-    string(REPLACE " " ";" CMAKE_EXE_LINKER_FLAGS_LIST
-                   "${CMAKE_EXE_LINKER_FLAGS}")
-    list(APPEND CGO_LDFLAGS "${CMAKE_EXE_LINKER_FLAGS_LIST}")
-  else()
-    list(APPEND CGO_LDFLAGS "")
-  endif()
-
-  if(CMAKE_C_LINK_FLAGS)
-    string(REPLACE " " ";" CMAKE_C_LINK_FLAGS_LIST "${CMAKE_C_LINK_FLAGS}")
-    list(APPEND CGO_LDFLAGS "${CMAKE_C_LINK_FLAGS_LIST}")
-  else()
-    list(APPEND CGO_LDFLAGS "")
-  endif()
-
-  if(CMAKE_BUILD_TYPE MATCHES debug)
-    string(REPLACE " " ";" CMAKE_C_FLAGS_DEBUG_LIST "${CMAKE_C_FLAGS_DEBUG}")
-    list(APPEND CGO_CFLAGS ${CMAKE_C_FLAGS_DEBUG_LIST})
-    string(REPLACE " " ";" CMAKE_EXE_LINKER_FLAGS_DEBUG_LIST
-                   "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
-    list(APPEND CGO_LDFLAGS "${CMAKE_EXE_LINKER_FLAGS_DEBUG_LIST}")
-  endif()
-
-  if(CMAKE_BUILD_TYPE MATCHES release)
-    string(REPLACE " " ";" CMAKE_C_FLAGS_RELEASE_LIST
-                   "${CMAKE_C_FLAGS_RELEASE}")
-    list(APPEND CGO_CFLAGS ${CMAKE_C_FLAGS_RELEASE_LIST})
-    string(REPLACE " " ";" CMAKE_EXE_LINKER_FLAGS_RELEASE_LIST
-                   "${CMAKE_EXE_LINKER_FLAGS_RELEASE}")
-    list(APPEND CGO_LDFLAGS "${CMAKE_EXE_LINKER_FLAGS_RELEASE_LIST}")
-  endif()
-
-  # Need to remove warnings for CGo to work
-  foreach(F ${CGO_CFLAGS_BLACKLIST})
-    list(REMOVE_ITEM CGO_CFLAGS "${F}")
-  endforeach()
-
-  # Add rpaths if present
-  foreach(R ${CGO_RPATHS})
-    list(APPEND CGO_LDFLAGS "-Wl,-rpath-link=${R}")
-  endforeach()
-
+            # "-l" flags for CGO
+            if(NOT -l${TARGET} IN_LIST ${LDFLAG_LIST})
+                list(APPEND ${LDFLAG_LIST} -l${TARGET})
+            endif()
+        endif()
+    endif()
 endmacro()
